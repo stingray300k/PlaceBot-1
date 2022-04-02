9633d1da4e0199bdb187bcf8223b476f4ea1f5da
stingray300k <stingray300k@inbox.lv>
2022-04-02 21:59:16 +0200

    Bump package version (0.1.1)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="PlaceBot-vim",
-    version="0.1.0",
+    version="0.1.1",
     packages=["place_bot_vim"],
     scripts=["scripts/place-vim"],
     install_requires=[
